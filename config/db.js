bf9e2b19deadc7c91cdc038470010964f22add25
vpk-11 <83803726+vpk-11@users.noreply.github.com>
2026-06-18 09:46:58 -0400

    Replace config package with dotenv and process.env

--- config/db.js
+++ config/db.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const { copyFileSync } = require('fs');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db,{
-            useNewUrlParser: true  // otherwise erros will be generated
-        });
-
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected...');
     } catch (err) {
         console.log(err.message);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
